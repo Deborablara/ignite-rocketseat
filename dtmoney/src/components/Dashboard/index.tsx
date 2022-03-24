ebc6d4a15d4f9cd8a82a3ce621df990a6f2c822f
Deborablara <debora.blara19@gmail.com>
2022-03-24 10:48:45 -0300

    created TransationsTable

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Summary } from '../Summary'
+import { TransationsTable } from '../TransactionsTable'
 
 import {Container} from './styles'
 
@@ -7,6 +8,7 @@ export function Dashboard(){
   return(
     <Container>
       <Summary/>
+      <TransationsTable/>
     </Container>
   )
 }
